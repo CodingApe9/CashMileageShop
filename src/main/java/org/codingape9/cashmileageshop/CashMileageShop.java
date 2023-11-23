8c3ef6f951a418fb9f77d126989a46debccf96e3
CodingApe9 <pos06098@gmail.com>
2023-11-23 22:19:59 +0900

    chore: 기본 세팅

--- src/main/java/org/codingape9/cashmileageshop/CashMileageShop.java
+++ src/main/java/org/codingape9/cashmileageshop/CashMileageShop.java
@@ -1,13 +1,22 @@
 package org.codingape9.cashmileageshop;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.codingape9.cashmileageshop.dto.UserDto;
+import org.codingape9.cashmileageshop.manager.MyBatisManager;
+import org.codingape9.cashmileageshop.mapper.PropertyManager;
+import org.codingape9.cashmileageshop.repository.UserRepository;
 
 public final class CashMileageShop extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        PropertyManager propertyManager = PropertyManager.getInstance();
+        propertyManager.createPluginFolder();
 
+
+
+        Bukkit.getConsoleSender().sendMessage("§4[CashMileageShop] 플러그인이 활성화되었습니다.");
     }
 
     @Override
