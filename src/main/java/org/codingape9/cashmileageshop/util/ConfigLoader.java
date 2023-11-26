a166d6c229996138ea8eb174dc0425bb9be227de
pwrwpw <dlgustjr9912@gmail.com>
2023-11-26 13:59:38 +0900

    refactor : 사용하지않는 import문 삭제

--- src/main/java/org/codingape9/cashmileageshop/util/ConfigLoader.java
+++ src/main/java/org/codingape9/cashmileageshop/util/ConfigLoader.java
@@ -2,7 +2,6 @@ package org.codingape9.cashmileageshop.util;
 
 import java.io.File;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.codingape9.cashmileageshop.CashMileageShop;
 
 public class ConfigLoader {
 
